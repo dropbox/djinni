4da0156099c8d180edaa6776fa805c19cd71547d
Steven Dee <steve@smartercode.net>
2015-01-13 21:49:23 -0500

    Make glob.py work with python3

--- example/glob.py
+++ example/glob.py
@@ -2,6 +2,8 @@
 # a helper for gyp to do file globbing cross platform
 # Usage: python glob.py root_dir pattern [pattern...]
 
+from __future__ import print_function
+
 import fnmatch
 import os
 import sys
@@ -15,4 +17,4 @@ for (dirpath, dirnames, files) in os.walk(root_dir):
         for pattern in patterns:
             match = match or fnmatch.fnmatch(f, pattern)
         if match:
-            print os.path.join(dirpath, f)
+            print(os.path.join(dirpath, f))
