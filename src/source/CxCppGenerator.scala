cb1d66969696ae9a635af23a4650bf92d5e698a5
Don Goodman-Wilson <don@slack-corp.com>
2015-07-17 09:22:49 -0700

    Fixed problem with cxcpp filenames being repeated and hence overwriting each other.

--- src/source/CxCppGenerator.scala
+++ src/source/CxCppGenerator.scala
@@ -170,7 +170,7 @@ class CxCppGenerator(spec: Spec) extends Generator(spec) {
 
     val helperClass = cxcppMarshal.helperClass(ident)
 
-    writeCxCppFile(cxName, origin, refs.privHeader, w => {
+    writeCxCppFile(cxcppMarshal.headerName(cxName), origin, refs.privHeader, w => {
       w.wl
       wrapNamespace(w, spec.cxcppNamespace, w => {
         w.wl(s"struct $helperClass")
@@ -186,7 +186,7 @@ class CxCppGenerator(spec: Spec) extends Generator(spec) {
       })
     })
 
-    writeCxCppFile(cxName, origin, refs.body, w => {
+    writeCxCppFile(cxcppMarshal.bodyName(cxName), origin, refs.body, w => {
       wrapNamespace(w, spec.cxcppNamespace, w => {
         w.wl(s"auto $helperClass::toCpp(CxType obj) -> CppType")
         w.braced {
