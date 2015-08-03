384943f066149b55bb76b1fd679b29f6fa5c1ec2
Miro Knejp <miro.knejp@gmail.com>
2015-08-03 07:34:21 +0200

    New "flags" archetype for enums oriented towards bitwise flags
    This generates enum values representing individual bits instead of
    integers. In C++ this also provides overloaded bitwise operators for
    convenient use of "enum class" types. ObjC generates NS_OPTIONS and in
    Java these special enums are supported via EnumSet.
    
    The overloaded operators are & | ^ &= |= ^= ~
    
    One can mark enum values with the tags "all" or "none" to generate
    special values. "none" creates a zero value and "all" a bitwise-or
    combination of all other values. These are absent form Java because
    EnumSet already provides the necessary facility.

--- src/source/JavaGenerator.scala
+++ src/source/JavaGenerator.scala
@@ -109,7 +109,7 @@ class JavaGenerator(spec: Spec) extends Generator(spec) {
       writeDoc(w, doc)
       javaAnnotationHeader.foreach(w.wl)
       w.w(s"public enum ${marshal.typename(ident, e)}").braced {
-        for (o <- e.options) {
+        for (o <- normalEnumOptions(e)) {
           writeDoc(w, o.doc)
           w.wl(idJava.enum(o.ident) + ",")
         }
@@ -238,7 +238,7 @@ class JavaGenerator(spec: Spec) extends Generator(spec) {
           for (f <- r.fields) {
             skipFirst { w.wl(",") }
             marshal.nullityAnnotation(f.ty).map(annotation => w.w(annotation + " "))
-            w.w(marshal.typename(f.ty) + " " + idJava.local(f.ident))
+            w.w(marshal.paramType(f.ty) + " " + idJava.local(f.ident))
           }
           w.wl(") {")
         }
@@ -254,7 +254,7 @@ class JavaGenerator(spec: Spec) extends Generator(spec) {
           w.wl
           writeDoc(w, f.doc)
           marshal.nullityAnnotation(f.ty).foreach(w.wl)
-          w.w("public " + marshal.typename(f.ty) + " " + idJava.method("get_" + f.ident.name) + "()").braced {
+          w.w("public " + marshal.returnType(Some(f.ty)) + " " + idJava.method("get_" + f.ident.name) + "()").braced {
             w.wl("return " + idJava.field(f.ident) + ";")
           }
         }
