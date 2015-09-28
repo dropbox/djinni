79462ca143b1badd2c9ff3332e249a4288746d1a
Jacob Potter <j4cbo@dropbox.com>
2015-09-28 12:15:24 -0700

    Use gcc 4.9 for example
    Fixes #146

--- example/android/app/jni/Application.mk
+++ example/android/app/jni/Application.mk
@@ -3,8 +3,8 @@
 APP_ABI := all
 APP_OPTIM := release
 APP_PLATFORM := android-8
-# GCC 4.8 Toolchain - requires NDK r9
-NDK_TOOLCHAIN_VERSION = 4.8
+# GCC 4.9 Toolchain - requires NDK r10
+NDK_TOOLCHAIN_VERSION = 4.9
 # GNU libc++ is the only Android STL which supports C++11 features
 APP_STL := gnustl_static
 APP_BUILD_SCRIPT := jni/Android.mk
