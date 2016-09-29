7f9bfef55bc561e668aaa201965d43c870488933
Andrew Twyman <atwyman@dropbox.com>
2016-09-28 21:53:42 -0700

    Add @autorelease pool in ObjC proxy destructors
    This is fixing a memory leak found in Dropbox's iOS networking libraries. Some ObjC objects were being autoreleased into a pool which is never flushed (the top-level pool on a C++-owned thread. This was Djinni violating its contracts around language isolation. The root cause turned out to be the destructor of an ObjcProxy object. Since it's called directly from C++, and does all its work in destructors, it was manipulating ObjC objects without an @autoreleasepool.
    
    The fix is more complex than I'd like due to the need to arrange for destructors to run inside of an @autorelease block, which forces an awkward code structure. Furthermore, I can't use optional without knowing what namespace to look in to find nullopt, which isn't easily extractable from Djinni's current usage, so I had to write my own. I'd very much like to find a simpler fix, so suggested follow-on diffs are welcome.

--- support-lib/objc/DJIObjcWrapperCache+Private.h
+++ support-lib/objc/DJIObjcWrapperCache+Private.h
@@ -53,4 +53,72 @@ static std::shared_ptr<CppType> get_objc_proxy(ObjcType * objcRef) {
     ));
 }
 
+// Private implementation base class for all ObjC proxies, which manages the
+// Handle, and ensures that it is created and destroyed in a safe way, inside
+// of an @autoreleasepool to avoid leaks.  Most of the complexity here is just
+// necessary to gain explicit control of construction and destruction time of
+// the member Handle, to make sure it's inside the @autoreleasepool block.
+template <typename ObjcType>
+class ObjcProxyBase {
+public:
+    ObjcProxyBase(ObjcType objc) {
+        @autoreleasepool {
+            m_djinni_private_proxy_handle.emplace(objc);
+        }
+    }
+
+    ObjcProxyBase(const ObjcProxyBase&) = delete;
+    ObjcProxyBase& operator=(const ObjcProxyBase&) = delete;
+
+    // Not intended for polymorphic use, so dtor is not virtual
+    ~ObjcProxyBase() {
+        @autoreleasepool {
+            m_djinni_private_proxy_handle.reset();
+        }
+    }
+
+    // Long name to minimize likelyhood of collision with interface methods.
+    ObjcType djinni_private_get_proxied_objc_object() const {
+        return m_djinni_private_proxy_handle.value().get();
+    }
+
+private:
+    // Simple stand-in for std::experimental::optional since the
+    // support-lib doesn't know which optional implementation might be in use.
+    // We can't take OptionalType as a template arg here, because there's no
+    // way to implement reset() without also knowing where to find nullopt, not
+    // just optional itself.  This class uses the same technique as at least
+    // one optional implementation: The union ensures proper alignment, while
+    // leaving construction and destruction under our direct control.
+    // This class is NOT safe for general use.  It's safe only if emplace() and
+    // reset() are each called exactly once.
+    class OptionalObjcProxyHandle final {
+    public:
+        using HandleType = ::djinni::ObjcProxyCache::Handle<ObjcType>;
+
+        OptionalObjcProxyHandle() {};
+        OptionalObjcProxyHandle(const OptionalObjcProxyHandle&) = delete;
+        OptionalObjcProxyHandle& operator=(const OptionalObjcProxyHandle&) = delete;
+        ~OptionalObjcProxyHandle() {};
+
+        void emplace(ObjcType objc) {
+            new (&m_handle) HandleType(objc);
+        }
+
+        void reset() {
+            m_handle.~HandleType();
+        }
+
+        const HandleType & value() const { return m_handle; }
+    private:
+        union {
+            char m_dummy;
+            HandleType m_handle;
+        };
+    };
+
+    // Long name to minimize likelyhood of collision with interface methods.
+    OptionalObjcProxyHandle m_djinni_private_proxy_handle;
+};
+
 } // namespace djinni
