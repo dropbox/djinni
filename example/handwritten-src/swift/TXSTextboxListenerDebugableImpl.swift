66f6d9b448a0bbab0f3c371adfe56959ef6d15e2
Bruno Coelho <4brunu@gmail.com>
2017-04-05 17:30:08 +0100

    Make Swift code more idiomatic

--- example/handwritten-src/swift/TXSTextboxListenerDebugableImpl.swift
+++ example/handwritten-src/swift/TXSTextboxListenerDebugableImpl.swift
@@ -1,24 +1,21 @@
-//
-//  TXSTextboxListenerImpl.swift
-//  TextSort
-//
-//  Created by Bruno Coelho on 10/03/2017.
-//  Copyright © 2017 Dropbox, Inc. All rights reserved.
-//
-
 import UIKit
 
-@objc class TXSTextboxListenerDebugableImpl : NSObject, TXSTextboxListener {
-    private var textView_: UITextView
+final class TXSTextboxListenerDebugableImpl : NSObject, TXSTextboxListener {
+    private var textView: UITextView
     
-    @objc(initWithUITextView:)
-    init (textView: UITextView) {
-        textView_ = textView
+    @available(*, unavailable)
+    override init() {
+        fatalError("Unsupported")
     }
     
-    @objc func update(_ items: TXSItemList) {
+    @objc(initWithUITextView:)
+    init(textView: UITextView) {
+        self.textView = textView
+    }
+    
+    func update(_ items: TXSItemList) {
         let string = items.items.joined(separator: "\n")
         print("TXSTextboxListenerDebugableImpl -> update \n\(string)")
-        textView_.text = string
+        textView.text = string
     }
 }
