28b6e9111d88017aa947d3c07c7123d23664eda5
Xianwen Chen <xianwen@dropbox.com>
2018-07-17 15:04:54 -0700

    Update generated source code

--- example/generated-src/java/com/dropbox/textsort/SortItems.java
+++ example/generated-src/java/com/dropbox/textsort/SortItems.java
@@ -36,14 +36,14 @@ import javax.annotation.Nonnull;
         }
 
         private native void nativeDestroy(long nativeRef);
-        public void destroy()
+        public void _djinni_private_destroy()
         {
             boolean destroyed = this.destroyed.getAndSet(true);
             if (!destroyed) nativeDestroy(this.nativeRef);
         }
         protected void finalize() throws java.lang.Throwable
         {
-            destroy();
+            _djinni_private_destroy();
             super.finalize();
         }
 
