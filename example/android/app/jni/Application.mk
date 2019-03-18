ca1cd82b60aab1d19835fe2ec07bc7d61a57a3f6
Alexandre Rocha Lima e Marcondes <alexandre.marcondes@keyidentity.com>
2019-03-18 16:53:25 +0100

    Updating the Makefile to be able to use newer Android NDK versions

--- example/android/app/jni/Application.mk
+++ example/android/app/jni/Application.mk
@@ -2,10 +2,7 @@
 
 APP_ABI := all
 APP_OPTIM := release
-APP_PLATFORM := android-8
-# GCC 4.9 Toolchain - requires NDK r10
-NDK_TOOLCHAIN_VERSION = 4.9
-# GNU libc++ is the only Android STL which supports C++11 features
-APP_STL := gnustl_static
+APP_PLATFORM := android-16
+APP_STL := c++_static
 APP_BUILD_SCRIPT := jni/Android.mk
 APP_MODULES := libtextsort_jni
